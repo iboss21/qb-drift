aa4874b14c9a11ef75f3d7162934fcf0eb097575
Master Mind <48193931+iboss21@users.noreply.github.com>
2023-08-15 21:50:21 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ author 'iBoss21'
 description 'QBCore Drift XP System'
 version '1.0.0'
 
-client_script 'client.lua'
-server_script 'server.lua'
+client_script 'client/client.lua'
+server_script 'server/server.lua'
 
 shared_script 'config.lua'
