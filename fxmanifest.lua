94f6fc6991a9d8261c42a4133a41148db7ee25ec
Master Mind <48193931+iboss21@users.noreply.github.com>
2023-08-15 21:44:42 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'iBoss21'
+description 'QBCore Drift XP System'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+shared_script 'config.lua'
