a5c17c9a9751dcf48d9ec29f3332dff18e092b37
Master Mind <48193931+iboss21@users.noreply.github.com>
2023-08-15 21:46:02 -0400

    Create server.lua

---
+++ server/server.lua
@@ -0,0 +1,23 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+QBCore.Functions.CreateCallback('qb-driftxp:getXP', function(source, cb)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    
+    if Player then
+        cb(Player.PlayerData.metadata['driftxp'] or 0)
+    else
+        cb(0)
+    end
+end)
+
+RegisterServerEvent('qb-driftxp:updateXP')
+AddEventHandler('qb-driftxp:updateXP', function(xp)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    
+    if Player then
+        Player.PlayerData.metadata['driftxp'] = xp
+        Player.Functions.SetMetaData("driftxp", Player.PlayerData.metadata['driftxp'])
+    end
+end)
